292e68f9d783425da848b91ca51081f46d17eaca
Nel Fernandes <46218259+imNel@users.noreply.github.com>
2023-09-04 00:43:25 +0800

    Remove unused

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,32 +3,22 @@ local wezterm = require 'wezterm'
 wezterm.on(
   'format-tab-title',
   function(tab, tabs, panes, config, hover, max_width)
-    local edge_background = '#191726'
-    local background = '#191726'
-    local foreground = '#e0def4'
+    local edge_background = '#24283B'
+    local background = '#24283B'
+    local foreground = '#565F89'
 
     local edge_foreground = background
     if tab.is_active then
-      background = '#569fba'
-      foreground = '#191726'
+      background = '#7AA2F7'
+      foreground = '#24283B'
       edge_background = background
-      edge_foreground = '#191726'
+      edge_foreground = '#24283B'
     end
 
     local title = wezterm.truncate_right(tab.active_pane.title, max_width - 2)
 
     local left_edge = ' '
     local right_edge = ' '
-
-    if tab.active_pane.title == 'nvim' then
-      title = ''
-    end
-    if tab.active_pane.title == 'yarn' then
-      title = ''
-    end
-    if tab.active_pane.title == 'lg' then
-      title = ''
-    end
 
     -- first tab
     if tab.tab_index == 0 then
@@ -53,20 +43,19 @@ wezterm.on(
   end
 )
 
-
 return {
-  color_scheme = "duskfox",
-  font_size = 14.0,
+  color_scheme = "Tokyo Night Storm",
+  font_size = 16.0,
   tab_bar_at_bottom = true,
   hide_tab_bar_if_only_one_tab = true,
   use_fancy_tab_bar = false,
   window_decorations = "RESIZE",
   show_scroll_bar = false,
   window_padding = {
-    left = 0,
-    right = 0,
+    left = 8,
+    right = 8,
     bottom = 0,
-    top = 20,
+    top = 8,
   },
   tab_max_width = 24,
   colors = {
@@ -76,10 +65,10 @@ return {
         fg_color = '#ffffff',
         intensity = "Bold",
       },
-      background = '#191726',
+      background = '#24283B',
       new_tab = {
-        bg_color = '#191726',
-        fg_color = '#191726',
+        bg_color = '#24283B',
+        fg_color = '#24283B',
       },
     },
   },
