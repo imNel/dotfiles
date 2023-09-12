702c1ef2e5a4224d860df3e95c9cd8784691f474
Nel Fernandes <46218259+imNel@users.noreply.github.com>
2023-09-12 17:03:15 +0800

    Add sway config

--- .wezterm.lua
+++ .wezterm.lua
@@ -50,7 +50,7 @@ return {
   hide_tab_bar_if_only_one_tab = true,
   use_fancy_tab_bar = false,
   window_decorations = "RESIZE",
-  show_scroll_bar = false,
+  enable_scroll_bar = false,
   window_padding = {
     left = 8,
     right = 8,
